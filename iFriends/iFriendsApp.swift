fc768171090c4b3068409c0797599884a7630ace
VIctor <colen.victor@icloud.com>
2022-01-13 11:00:08 -0300

    Initial Commit

---
+++ iFriends/iFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFriendsApp.swift
+//  iFriends
+//
+//  Created by Victor Colen on 13/01/22.
+//
+
+import SwiftUI
+
+@main
+struct iFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
